26c6a11ac66ecc516d63d0de283d313f5bcc9a4a
Maricela <lmendez@adia.com.co>
2018-07-08 19:07:22 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,142 @@
+
+window.onload = function(){
+	calculadora.inicia()
+}
+
+var teclas = document.getElementsByClassName('tecla')
+var pantalla = document.getElementById('display')
+var numero = pantalla.innerText
+var dos =0;
+var operacion = 0;
+var operacionGuardada = '';
+var verificar = 0;
+
+var calculadora = {
+
+	inicia : function(){
+		this.presionar()
+
+	},
+
+	presionar : function(){
+
+		for (var i = 0; i < teclas.length; i++) {
+
+			teclas[i].addEventListener('mousedown', function(){
+				this.style.border= '3px solid #999999';
+				this.style.borderRadius= '0px';
+				var digito = this.id
+				if(!isNaN(digito)){
+					calculadora.nuevoNumero(digito);
+				}else{
+					calculadora.opera(digito)
+				}
+			})
+			teclas[i].addEventListener('mouseup', function(){
+				this.style.border= '0px';
+			})
+		}
+	},
+
+	nuevoNumero : function(digito){
+
+		if(verificar == 1){
+			this.reiniciar();
+		}
+		var newNumber = digito
+		numero = pantalla.innerText
+		if(numero.length<8){
+			if(numero == 0 && newNumber !== 0 && numero != '0.' ){
+				pantalla.innerText = newNumber
+			}
+			if(numero != 0 && newNumber !== 0 || numero == '0.'){
+				pantalla.innerText = numero+newNumber
+			}
+			verificar = 0
+		}
+	},
+
+	opera : function(operador){
+		if(operador == 'On'){
+			this.reiniciar();
+		}else if(operador == 'punto'){
+			this.decimal();
+		}else if(operador == 'signo'){
+			this.negativos()
+		}else{
+			this.operaciones(operador);
+		}
+	},
+
+	operaciones: function(operador){
+		numero = pantalla.innerText
+		pantalla.innerText = ''
+		if(operacion == 0){
+			operacion = +numero
+			operacionGuardada = operador
+		}else{
+			switch(operacionGuardada){
+				case 'mas':
+					operacion = +operacion + +numero;
+					break;
+				case 'menos':
+					operacion = +operacion - +numero;
+					break;
+				case 'por':
+					operacion = +operacion * +numero;
+					break;
+				case 'dividido':
+					operacion = +operacion / +numero;
+					break;
+			}
+		}
+
+		if(operador == 'igual'){
+      dos = dos +1;
+      console.log('se ha presionado el igual '+ dos + ' veces');
+      console.log(operador);
+			operacion = this.valida(operacion)
+      console.log(operacion);
+			pantalla.innerText = operacion
+			operacion = 0;
+			verificar = 1;
+		}else{
+			verificar = 0
+			operacionGuardada = operador
+     		}
+  	},
+
+	reiniciar : function(){
+		pantalla.innerText = 0;
+		operacion = 0;
+	},
+
+	decimal : function(){
+		numero = pantalla.innerText
+		if(numero.length<8){
+			var esDec = Number(numero)
+			if(Number.isInteger(esDec) && numero != '0.'){
+				pantalla.innerText = numero+'.'
+			}
+		}
+	},
+
+	negativos : function(){
+		numero = pantalla.innerText
+		if(numero != 0){
+			if(numero<0){
+				pantalla.innerText = numero.substring(1)
+			}else{
+					pantalla.innerText = '-'+pantalla.innerText
+			}
+		}
+	},
+
+	valida : function(result){
+		result = result.toString()
+		if(result.length > 8){
+			result = result.substr(0,8)
+		}
+    	return result
+	},
+}
